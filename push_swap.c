d32bbb93048ca5accf7bd02eb8559d14b72bd41c
SeungMinLee <seunlee2@student.42seoul.kr>
2023-09-05 17:35:07 +0900

    .

--- push_swap.c
+++ push_swap.c
@@ -6,13 +6,32 @@
 /*   By: seunlee2 <seunlee2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 15:47:39 by seunlee2          #+#    #+#             */
-/*   Updated: 2023/08/28 15:51:06 by seunlee2         ###   ########.fr       */
+/*   Updated: 2023/09/05 15:35:58 by seunlee2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(void)
+void	ft_error(int code)
 {
+	write(2, "Error\n", 6);
+	exit(code);
+}
+
+int	ft_chk_arg(t_ps *ps, int argc, char **argv)
+{
+
+}
+
+int	main(int argc, char **argv)
+{
+	t_ps	*ps;
+
+	if (argc < 2)
+		ft_error(1);
+	ps = (t_ps *)malloc(sizeof(t_ps));
+	if (!ps)
+		ft_error(1);
+	ft_chk_arg(ps, argc, argv);
 	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: seunlee2 <seunlee2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 15:47:55 by seunlee2          #+#    #+#             */
-/*   Updated: 2023/08/28 17:45:49 by seunlee2         ###   ########.fr       */
+/*   Updated: 2023/09/05 15:37:14 by seunlee2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 # define PUSH_SWAP_H
 
 # include "libft/libft.h"
+
+typedef struct s_l {
+	int			data;
+	struct s_l	*next;
+}	t_l;
+
+typedef struct s_ps {
+	t_l		*s_a;
+	t_l		*s_b;
+}	t_ps;
 
 #endif
